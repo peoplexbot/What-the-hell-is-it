1734b008152fc3c3234520058973498a75fcdff4
peoplexbot <hello@peoplexbot.com>
2025-04-07 23:53:19 -0700

    Delete project/app/(tabs) directory

--- project/app/(tabs)/hello/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HelloScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello from test screen!</Text>
-    </View>
-  );
-}
