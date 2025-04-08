a75f3a3aece78f0d0b161bce312969707623380e
peoplexbot <hello@peoplexbot.com>
2025-04-08 00:20:00 -0700

    Update api.ts

--- app/constants/api.ts
+++ app/constants/api.ts
@@ -1,1 +1,2 @@
-export const PUZZLE_API_URL = 'https://script.google.com/macros/s/AKfycbzOa9ETv1K3tozEtBuGfR_vND5uAWXuVcVMM1kb-ggH-iUhNeLcSaT97D910rxGqFi1sQ/exec';
+export const API_URL =
+  'https://script.google.com/macros/s/AKfycbzOa9ETv1K3tozEtBuGfR_vND5uAWXuVcVMM1kb-ggH-iUhNeLcSaT97D910rxGqFi1sQ/exec';
