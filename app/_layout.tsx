f3ff748223de305d44810530d6d6cec8162eac84
peoplexbot <hello@peoplexbot.com>
2025-04-08 11:37:51 -0700

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,13 @@
 import { Text, View } from 'react-native';
 import { useEffect, useState } from 'react';
-import { fetchPuzzle } from '../lib/getPuzzle'; // Adjust this if needed
+import { getPuzzle } from '../lib/getPuzzle'; // Adjust this if needed
 
 export default function Layout() {
   const [puzzle, setPuzzle] = useState<any>(null);
   const [error, setError] = useState('');
 
   useEffect(() => {
-    fetchPuzzle()
+    getPuzzle()
       .then(setPuzzle)
       .catch((err) => setError(err.message));
   }, []);
